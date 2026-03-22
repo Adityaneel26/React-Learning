7d8c3df5b7935b92ee0a60f287975837d64efa55
Aditya <adityaneel555@gmail.com>
2026-03-22 10:26:05 +0530

    onclick and mouseover events handling

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,11 +1,14 @@
-function doSomething(){
+function doSomething() {
     console.log("hello world")
 }
-
-export default function Button(){
-    return(
+function bye(){
+    console.log("bye this is end")
+}
+export default function Button() {
+    return (
         <div>
-            <button>click me</button>
+            <button onClick={doSomething}>click me</button>
+            <p onMouseOver={bye}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde doloribus necessitatibus minus, consequatur eligendi porro pariatur sit doloremque id sequi deserunt a magni.</p>
         </div>
     )
 }
